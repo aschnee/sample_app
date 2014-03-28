efdd6281fd01822afd503944705eff1dbc084e40
Eitan Aschner <eitan.aschner@gmail.com>
2014-03-28 12:49:20 +0800

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+
+    it "should have the Title 'Ruby on Rails Tutorial Sample App | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
     end
   end
 
@@ -14,6 +20,11 @@ describe "Static pages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+
+  	it "should have the Title 'Ruby on Rails Tutorial Sample App | Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+  	end
   end
 
   describe "About page" do
@@ -21,6 +32,11 @@ describe "Static pages" do
   	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
+  	end
+
+  	it "should have the Title 'Ruby on Rails Tutorial Sample App | About Us'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
   	end
   end
 end
